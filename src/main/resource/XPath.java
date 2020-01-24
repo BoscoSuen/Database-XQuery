0b22b4b97bf81ecb2b80b13ea2c8c3690b5b625f
Yukinichi <boscosuen@outlook.com>
2020-01-23 18:11:14 -0800

    visitor

--- src/main/resource/XPath.java
+++ src/main/resource/XPath.java
@@ -3,7 +3,10 @@ package main.resource;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.io.IOException;
 
@@ -16,6 +19,9 @@ public class XPath {
         xQueryParser xQueryParser = new xQueryParser(commonTokenStream);
 
         ParseTree parseTree = xQueryParser.ap();
-        System.out.println("done");
+        xQueryBaseVisitor visitor = new xQueryBaseVisitor();
+        visitor.visit(parseTree);
+        // can add a break point at the println to see the elements in the parseTree
+        System.out.println("test end here");
     }
 }
